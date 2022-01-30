60c2017f3fa6b509a0832dbd396d6500d561b711
lithander <lithander@gmx.de>
2022-01-30 02:21:34 +0100

    Changed the Evaluation to good old Chilli that MMC ran before I introduced the mobility tables. Should be worth a few ELO as it was tuned without the 13th table being preset.

--- Leorik.Engine/Program.cs
+++ Leorik.Engine/Program.cs
@@ -6,7 +6,7 @@ namespace Leorik.Engine
 {
     public static class Program
     {
-        const string NAME_VERSION = "Leorik 0.1";
+        const string NAME_VERSION = "Leorik 0.1.1";
         const string AUTHOR = "Thomas Jahn";
 
         static Engine _engine = new Engine();
