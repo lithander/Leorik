23546fa4f8f02d7646dfaad896e311e9829e5158
lithander <lithander@gmx.de>
2022-07-16 10:56:42 +0200

    Complete rewrite if the time-control logic to fix an issue where Leorik was running out of time in matches with no increment.

--- Leorik.Engine/TimeControl.cs
+++ Leorik.Engine/TimeControl.cs
@@ -6,19 +6,17 @@ namespace Leorik.Engine
     class TimeControl
     {
         const int BASE_MARGIN = 5;
-        const int BRANCHING_FACTOR_ESTIMATE = 3;
-        const int MAX_TIME_REMAINING = int.MaxValue / 3; //large but not too large to cause overflow issues
+        const int MAX_TIME = int.MaxValue / 3; //large but not too large to cause overflow issues
+        const float PLAY_ON_INC = 0.5f;
+        const float PLAY_ON_RESERVE = 0.85f;
+        const float ABORT_RATIO = 0.5f;
 
-        private int _movesToGo;
-        private int _increment;
-        private int _remaining;
-        private int _margin;
+        private int _moveTime;
+        private int _moveTimeLimit;
         private int _maxDepth;
 
         private long _t0 = -1;
         private long _tN = -1;
-
-        public int TimePerMove() => (_remaining + (_movesToGo - 1) * _increment) / _movesToGo;
 
         private long Now => Stopwatch.GetTimestamp();
         public int Elapsed => MilliSeconds(Now - _t0);
@@ -32,12 +30,9 @@ namespace Leorik.Engine
 
         private void Reset()
         {
-            _movesToGo = 1;
-            _increment = 0;
-            _remaining = MAX_TIME_REMAINING;
+            _moveTimeLimit = MAX_TIME;
             _t0 = Now;
             _tN = _t0;
-            _margin = BASE_MARGIN;
         }
 
         public void StartInterval()
@@ -48,23 +43,27 @@ namespace Leorik.Engine
         public void Stop()
         {
             //this will cause CanSearchDeeper() and CheckTimeBudget() to evaluate to 'false'
-            _remaining = 0;
+            _moveTimeLimit = 0;
         }
 
         internal void Go(int maxDepth, int timePerMove)
         {
             Reset();
             _maxDepth = maxDepth;
-            _remaining = Math.Min(timePerMove, MAX_TIME_REMAINING);
+            _moveTimeLimit = _moveTime = Math.Min(timePerMove, MAX_TIME);
         }
 
         internal void Go(int maxDepth, int time, int increment, int movesToGo)
         {
             Reset();
             _maxDepth = maxDepth;
-            _remaining = Math.Min(time, MAX_TIME_REMAINING);
-            _increment = increment;
-            _movesToGo = movesToGo;
+            int futureMoves = movesToGo - 1;
+            int timeRemaining = Math.Min(time, MAX_TIME) + futureMoves * increment;
+            _moveTime = Math.Min(time, timeRemaining / movesToGo);
+            //abort as soon as the remaining moves have each less than 'reserve_ratio' of this one
+            float reserveRatio = increment > 50 ? PLAY_ON_INC : PLAY_ON_RESERVE;
+            int reserve = (int)(futureMoves * _moveTime * reserveRatio);
+            _moveTimeLimit = Math.Min(time, timeRemaining - reserve);
         }
 
         public bool CanSearchDeeper(int depth)
@@ -72,36 +71,19 @@ namespace Leorik.Engine
             if (depth >= _maxDepth)
                 return false;
 
-            int budget = TimePerMove();
+            _moveTimeLimit -= BASE_MARGIN;
 
-            //we use a dynamic margin based on the depth, but at least 10% of the time per move
-            _margin = Math.Max(BASE_MARGIN * depth, budget / 10);
-
-            //estimate the branching factor, if only one move to go we yolo with a low estimate
-            int elapsed = Elapsed;
-            int multi = (_movesToGo == 1) ? 1 : BRANCHING_FACTOR_ESTIMATE;
-            float overdraft = (_increment == 0) ? 1.5f : 2.0f;
-            int estimate = multi * ElapsedInterval;
-            int total = elapsed + estimate + _margin;
-
-            //we have already exceeded the budget for the move
-            if (elapsed > budget)
-                return false;
-            //shouldn't spend more than X% of the per move budget on a single move
-            if (total > overdraft * budget)
-                return false;
-            //can't afford the estimate
-            if (total > _remaining - _margin)
+            if (Elapsed > _moveTime * ABORT_RATIO)
                 return false;
 
-            //all conditions fulfilled - let's base the margin on the budget
+            //all conditions fulfilled
             return true;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public bool CheckTimeBudget()
         {
-            return Elapsed > (_remaining - _margin);
+            return Elapsed > _moveTimeLimit;
         }
     }
 }
