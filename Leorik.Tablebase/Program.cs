6ecde41e6b29629ea6843f5cfde0e25cac0b7add
lithander <lithander@gmx.de>
2022-11-09 22:24:16 +0100

    Added a new project to the solution that compiles to a console application probing the WDL tables for any given FEN and printing lots of debug information. Heavy WIP.

---
+++ Leorik.Tablebase/Program.cs
@@ -0,0 +1,57 @@
+﻿using Leorik.Core;
+using System;
+
+namespace Leorik.Tablebase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine(" Leorik Syzygy v1  ");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine();
+
+            string tbPath = GetPath(args);
+            Console.WriteLine($"Syzygy Tablebase Path: {tbPath}");
+            Syzygy tb = new Syzygy(tbPath);
+
+            Console.WriteLine();
+            string fen = GetPosition(args);
+            do
+            {
+                Probe(tb, fen);
+                fen = Console.ReadLine();
+            }
+            while(!string.IsNullOrEmpty(fen));
+        }
+
+        private static void Probe(Syzygy tb, string fen)
+        {
+            BoardState pos = Notation.GetBoardState(fen);
+            if (tb.ProbeWinDrawLoss(pos, out WinDrawLoss result))
+                Console.WriteLine($"ProbeWinDrawLoss(...) = {(int)result} = {result}");
+            else
+                Console.WriteLine("ProbeWinDrawLoss(...) failed!");
+        }
+
+        private static string GetPosition(string[] args)
+        {
+            if (args.Length > 1)
+                return args[1];
+
+            Console.Write($"Enter FEN: ");
+            return Console.ReadLine();
+
+        }
+
+        private static string GetPath(string[] args)
+        {
+            if (args.Length > 0)
+                return args[0];
+
+            Console.Write($"Provide the Syzygy Tablebase Path: ");
+            return Console.ReadLine();
+        }
+    }
+}
