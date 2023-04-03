35b2fb9bff4a546b8c711879c72e84d1aa897689
lithander <lithander@gmx.de>
2023-04-04 01:21:19 +0200

    KiSS simplified further, quite happy with the current state

--- Leorik.Core/KiSS.cs
+++ Leorik.Core/KiSS.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using System.Numerics;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.Intrinsics.X86;
+﻿using System.Runtime.CompilerServices;
 
 namespace Leorik.Core
 {
@@ -11,16 +6,19 @@ namespace Leorik.Core
     //...henceforth we shall abreviate it as KiSS <3
     public static class KiSS
     {
-        static ulong[] DiagonalMask = new ulong[64];
-        static ulong[] AntiDiagonalMask = new ulong[64];
+        static readonly ulong[] DiagonalMask = new ulong[64];
+        static readonly ulong[] AntiDiagonalMask = new ulong[64];
 
-        static ulong[] RookSubset = new ulong[2 * 64 * 64];
-        static ulong[] BishopSubset = new ulong[2 * 64 * 64];
+        static readonly ulong[] RookSubset = new ulong[2 * 64 * 64];
+        static readonly ulong[] BishopSubset = new ulong[2 * 64 * 64];
 
         const int FILE_A = 0;
         const int FILE_H = 7;
         const int RANK_1 = 0;
         const int RANK_8 = 7;
+
+        const ulong FILE_A2_A7 = 0x0001010101010100;
+        const ulong DIAGONAL_C2_H7 = 0x0080402010080400;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         static int Rank(int square) => square >> 3;
@@ -28,16 +26,52 @@ namespace Leorik.Core
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         static int File(int square) => square & 7;
 
-        static bool IsBlocked(ulong mask, int square) => (mask & (1UL << File(square))) > 0;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ulong BishopAttacks(ulong occupation, int square)
+        {
+            ulong offset = (ulong)(square << 7);
+            ulong dIndex = ((occupation & DiagonalMask[square]) * FILE_A2_A7) >> 57;
+            ulong aIndex = ((occupation & AntiDiagonalMask[square]) * FILE_A2_A7) >> 57;
+            return BishopSubset[offset + dIndex] | BishopSubset[offset + 64 + aIndex];
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ulong RookAttacks(ulong occupation, int square)
+        {
+            ulong offset = (ulong)(square << 7);
+            ulong hIndex = (occupation >> ((square & 0b111000) | 1)) & 63;
+            ulong vIndex = (((occupation >> (square & 0b000111)) & FILE_A2_A7) * DIAGONAL_C2_H7) >> 58;
+            return RookSubset[offset + hIndex] | RookSubset[offset + 64 + vIndex];
+        }
+
+        //Table initialization
 
         static KiSS()
         {
-            InitDiagonalMasks();
-            InitSubsets();
-        }
+            //Init Masks
+            for (int square = 0; square < 64; square++)
+            {
+                int x = File(square);
+                int y = Rank(square);
 
-        private static void InitSubsets()
-        {
+                //set diagonal bits above square
+                for (int dx = x + 1, dy = y + 1; dx < FILE_H && dy < RANK_8; dx++, dy++)
+                    DiagonalMask[square] |= 1UL << (dx + dy * 8);
+
+                //set diagonal bits below square
+                for (int dx = x - 1, dy = y - 1; dx > FILE_A && dy > RANK_1; dx--, dy--)
+                    DiagonalMask[square] |= 1UL << (dx + dy * 8);
+
+                //set anti-diagonal bits above square
+                for (int dx = x - 1, dy = y + 1; dx > FILE_A && dy < RANK_8; dx--, dy++)
+                    AntiDiagonalMask[square] |= 1UL << (dx + dy * 8);
+
+                //set anti-diagonals bits below square
+                for (int dx = x + 1, dy = y - 1; dx < FILE_H && dy > RANK_1; dx++, dy--)
+                    AntiDiagonalMask[square] |= 1UL << (dx + dy * 8);
+            }
+
+            //Init Subsets
             for (int square = 0; square < 64; square++)
             {
                 int offset = square * 128;
@@ -51,133 +85,87 @@ namespace Leorik.Core
             }
         }
 
-        private static void InitDiagonalMasks()
-        {
-            for (int square = 0; square < 64; square++)
-            {
-                int x = File(square);
-                int y = Rank(square);
-
-                //set diagonal bits above square
-                for (int bit = square + 9, dx = x + 1, dy = y + 1; dx < FILE_H && dy < RANK_8; bit += 9, dx++, dy++)
-                    DiagonalMask[square] |= 1UL << bit;
-
-                //set diagonal bits below square
-                for (int bit = square - 9, dx = x - 1, dy = y - 1; dx > FILE_A && dy > RANK_1; bit -= 9, dx--, dy--)
-                    DiagonalMask[square] |= 1UL << bit;
-
-                //set anti-diagonal bits above square
-                for (int bit = square + 7, dx = x - 1, dy = y + 1; dx > FILE_A && dy < RANK_8; bit += 7, dx--, dy++)
-                    AntiDiagonalMask[square] |= 1UL << bit;
-
-                //set anti-diagonals bits below square
-                for (int bit = square - 7, dx = x + 1, dy = y - 1; dx < FILE_H && dy > RANK_1; bit -= 7, dx++, dy--)
-                    AntiDiagonalMask[square] |= 1UL << bit;
-            }
-        }
-
         static ulong GetDiagonalSubset(int square, int index)
         {
+            ulong blockers = GetBlockers(square, index);
             ulong result = 0;
-            ulong blockers = (ulong)(index << 1);
-
-            for (int sq = square; !IsBlocked(blockers, sq) && File(sq) < FILE_H && Rank(sq) < RANK_8; sq += 9)
+            for (int sq = square; IsFree(blockers, sq) && File(sq) < FILE_H && Rank(sq) < RANK_8; sq += 9)
                 result |= 1UL << (sq + 9);
-
-            for (int sq = square; !IsBlocked(blockers, sq) && File(sq) > FILE_A && Rank(sq) > RANK_1; sq -= 9)
+            for (int sq = square; IsFree(blockers, sq) && File(sq) > FILE_A && Rank(sq) > RANK_1; sq -= 9)
                 result |= 1UL << (sq - 9);
-
             return result;
         }
 
         static ulong GetAntiDiagonalSubset(int square, int index)
         {
+            ulong blockers = GetBlockers(square, index);
             ulong result = 0;
-            ulong blockers = (ulong)(index << 1);
-
-            for (int sq = square; !IsBlocked(blockers, sq) && File(sq) > FILE_A && Rank(sq) < RANK_8; sq += 7)
+            for (int sq = square; IsFree(blockers, sq) && File(sq) > FILE_A && Rank(sq) < RANK_8; sq += 7)
                 result |= 1UL << (sq + 7);
-
-            for (int sq = square; !IsBlocked(blockers, sq) && File(sq) < FILE_H && Rank(sq) > RANK_1; sq -= 7)
+            for (int sq = square; IsFree(blockers, sq) && File(sq) < FILE_H && Rank(sq) > RANK_1; sq -= 7)
                 result |= 1UL << (sq - 7);
-
             return result;
         }
 
         static ulong GetHorizontalSubset(int square, int index)
         {
+            ulong blockers = GetBlockers(square, index);
             ulong result = 0;
-            ulong blockers = (ulong)(index << 1);
-            //unblock the starting square
-            blockers &= ~(1UL << File(square));
-
-            for (int sq = square; !IsBlocked(blockers, sq) && File(sq) < FILE_H; sq++)
+            for (int sq = square; IsFree(blockers, sq) && File(sq) < FILE_H; sq++)
                 result |= 1UL << (sq + 1);
-
-            for (int sq = square; !IsBlocked(blockers, sq) && File(sq) > FILE_A; sq--)
+            for (int sq = square; IsFree(blockers, sq) && File(sq) > FILE_A; sq--)
                 result |= 1UL << (sq - 1);
-
             return result;
         }
 
         static ulong GetVerticalSubset(int square, int index)
         {
-            // vertical indexes
+            ulong blockers = GetVerticalBlockers(square, index);
             ulong result = 0;
-            ulong blockers = 0;
-            for (int i = 0; i <= 5; i++)
-            {
-                if ((index & (1 << i)) > 0)
-                {
-                    int shift = ((5 - i) << 3) + 8;
-                    blockers |= 1UL << shift;
-                }
-            }
-            for (int sq = square; Rank(sq) < RANK_8; sq += 8)
-            {
-                int up = sq + 8;
-                result |= 1UL << up;
-                if ((blockers & (1UL << (up - File(up)))) > 0) 
-                    break;
-            }
-            for (int sq = square; Rank(sq) > RANK_1; sq -= 8)
-            {
-                int down = sq - 8;
-                result |= 1UL << down;
-                if ((blockers & (1UL << (down - File(down)))) > 0) 
-                    break;
-            }
+            for (int sq = square; IsVerticalFree(blockers, sq) && Rank(sq) < RANK_8; sq += 8)
+                result |= 1UL << (sq + 8);
+            for (int sq = square; IsVerticalFree(blockers, sq) && Rank(sq) > RANK_1; sq -= 8)
+                result |= 1UL << (sq - 8);
             return result;
         }
 
-        const ulong FILE_B2_B7 = 0x0002020202020200;
-        const ulong FILE_A2_A7 = 0x0001010101010100;
-        const ulong DIAGONAL_C2_H7 = 0x0080402010080400;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static bool IsFree(ulong mask, int square) => (mask & (1UL << File(square))) == 0;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ulong BishopAttacks(ulong occupation, int square)
+        static ulong GetBlockers(int square, int index) => (ulong)(index << 1) & ~(1UL << File(square));
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static bool IsVerticalFree(ulong mask, int square) => (mask & (1UL << (square - File(square)))) == 0;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static ulong GetVerticalBlockers(int square, int index)
         {
-            ulong offset = (ulong)(square << 7);
-            ulong dIndex = ((occupation & DiagonalMask[square]) * FILE_B2_B7) >> 58;
-            ulong aIndex = ((occupation & AntiDiagonalMask[square]) * FILE_B2_B7) >> 58;
-            return BishopSubset[offset + dIndex] | BishopSubset[offset + 64 + aIndex];
+            //Place the 6 'index' bits FEDCBA like this, leave standing square ZERO 
+            //  - - - - - - - - 
+            //  A - - - - - - - 
+            //  B - - - - - - - 
+            //  C - - - - - - - 
+            //  D - - - - - - - 
+            //  E - - - - - - - 
+            //  F - - - - - - - 
+            //  - - - - - - - - 
+            ulong blockers = 0;
+            for (int i = 0, shift = 48; i < 6; i++, shift -= 8)
+                if ((index & (1 << i)) > 0) //index bit is set?
+                    if (shift != square - File(square)) //don't block standing square
+                        blockers |= 1UL << shift;
+            return blockers;
         }
+
+        //~~~ DEBUG ~~~
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ulong _BishopAttacks(ulong occupation, int square)
         {
-            ulong dIndex = ((occupation & DiagonalMask[square]) * FILE_B2_B7) >> 58;
-            ulong aIndex = ((occupation & AntiDiagonalMask[square]) * FILE_B2_B7) >> 58;
+            ulong dIndex = ((occupation & DiagonalMask[square]) * FILE_A2_A7) >> 57;
+            ulong aIndex = ((occupation & AntiDiagonalMask[square]) * FILE_A2_A7) >> 57;
             return GetDiagonalSubset(square, (int)dIndex) | GetAntiDiagonalSubset(square, (int)aIndex);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ulong RookAttacks(ulong occupation, int square)
-        {
-            ulong offset = (ulong)(square << 7);
-            ulong hIndex = (occupation >> ((square & 0b111000) | 1)) & 63;
-            ulong vIndex = (((occupation >> (square & 0b000111)) & FILE_A2_A7) * DIAGONAL_C2_H7) >> 58;
-            return RookSubset[offset + hIndex] | RookSubset[offset + 64 + vIndex];
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
