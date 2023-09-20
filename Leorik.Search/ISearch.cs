7d09e2dccdab5d9649d7d45f45c7905dc5d5853a
lithander <lithander@gmx.de>
2023-09-20 02:27:55 +0200

    Moved ISearch and ParallelSearch into their own files!
    A short match 8 vs 1 Threads at 5s + 200ms indicated 165 Elo advantage!
    
    conf="Leorik-2.4.7c" option.Threads=8 -each tc=5+0.2 book=varied.bin option.Hash=64
    Score of Leorik-2.4.7c vs Leorik-2.4.7: 165 - 24 - 129  [0.722] 318
    Elo difference: 165.5 +/- 30.1, LOS: 100.0 %, DrawRatio: 40.6 %

---
+++ Leorik.Search/ISearch.cs
@@ -0,0 +1,41 @@
+﻿using Leorik.Core;
+
+namespace Leorik.Search
+{
+    public struct SearchOptions
+    {
+        public int MidgameRandomness;
+        public int EndgameRandomness;
+        public long MaxNodes;
+        public int NullMoveCutoff;
+        public int Threads;
+
+        internal readonly int Randomness(float phase)
+        {
+            return Math.Max(0, (int)(MidgameRandomness + (EndgameRandomness - MidgameRandomness) * phase));
+        }
+
+        public readonly static SearchOptions Default = new();
+
+
+        public SearchOptions()
+        {
+            Threads = 1;
+            MaxNodes = long.MaxValue;
+            MidgameRandomness = 0;
+            EndgameRandomness = 0;
+            NullMoveCutoff = 338;
+        }
+    }
+
+    public interface ISearch
+    {
+        bool Aborted { get; }
+        int Depth { get; }
+        int Score { get; }
+        long NodesVisited { get; }
+        Span<Move> PrincipalVariation { get; }
+        void SearchDeeper(Func<bool> checkTimeBudget);
+        void Search(int depth);
+    }
+}
---
+++ Leorik.Search/ParallelSearch.cs
@@ -0,0 +1,49 @@
+﻿using Leorik.Core;
+
+namespace Leorik.Search
+{
+    public class ParallelSearch : ISearch
+    {
+        List<IterativeSearch> _worker = new List<IterativeSearch>();
+        int _best = 0;
+
+        public bool Aborted => _worker[_best].Aborted;
+        public int Depth => _worker[_best].Depth;
+        public int Score => _worker[_best].Score;
+        public long NodesVisited => _worker[_best].NodesVisited;
+        public Span<Move> PrincipalVariation => _worker[_best].PrincipalVariation;
+
+
+        public ParallelSearch(BoardState board, SearchOptions options, IEnumerable<BoardState> history)
+        {
+            for (int i = 0; i < options.Threads; i++)
+            {
+                var worker = new IterativeSearch(board, options, history);
+                worker.Schuffle();
+                _worker.Add(worker);
+            }
+        }
+
+        public void SearchDeeper(Func<bool>? killSwitch = null)
+        {
+            // Using a lambda expression.
+            Parallel.For(0, _worker.Count, i =>
+            {
+                _worker[i].SearchDeeper(killSwitch);
+                _best = i;
+            });
+        }
+
+        public void Search(int maxDepth)
+        {
+            // Using a lambda expression.
+            Parallel.For(0, _worker.Count, i =>
+            {
+                while (_worker[i].Depth < maxDepth)
+                    _worker[i].SearchDeeper(null);
+
+                _best = i;
+            });
+        }
+    }
+}
