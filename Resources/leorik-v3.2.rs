895009f5d808b27d8222bc85160b80fabe46e6d9
lithander <lithander@gmx.de>
2025-06-09 12:05:16 +0200

    Trained a new (768 -> 640)x2 -> 1 network on 6.1B positions including ~1B FRC and DFRC data. It is the same architecture and size that Leorik-3.1 was released with. It shows a small improvement in standard playing strength so it's likely the best net for that version!
    tc=40/15 vs 640HL-S-5288M-Tmix-Q5-v19.nnue!
    Score of Leorik-3.1.2 Test vs Leorik-3.1.2: 2403 - 2280 - 5317  [0.506] 10000
    Elo difference: 4.3 +/- 4.7, LOS: 96.4 %, DrawRatio: 53.2 %
    
    Added the bullet file this network was trained with: leorik-v3.1.rs
    Also added leorik-v3.2.rs which is creates a horizontally mirrored net with output and king buckets and is compatible with current master version 3.1.8

---
+++ Resources/leorik-v3.2.rs
@@ -0,0 +1,68 @@
+use bullet_lib::{
+    default::{inputs, loader, outputs, Loss, TrainerBuilder},
+    lr, optimiser,
+    wdl, Activation, LocalSettings, TrainingSchedule, TrainingSteps,
+};
+
+const INPUT_BUCKETS: [usize; 32] = [
+	0, 0, 1, 1,
+	2, 2, 3, 3,
+	2, 2, 3, 3,
+	4, 4, 4, 4,
+	4, 4, 4, 4,
+	4, 4, 4, 4,
+	4, 4, 4, 4,
+	4, 4, 4, 4,
+];
+const OUTPUT_BUCKETS: usize = 8;
+const HIDDEN_SIZE: usize = 640;
+const SCALE: i32 = 400;
+const QA: i16 = 255;
+const QB: i16 = 64;
+
+fn main() {
+    let mut trainer = TrainerBuilder::default()
+        .quantisations(&[QA, QB])
+        .optimiser(optimiser::AdamW)
+        .loss_fn(Loss::SigmoidMSE)
+        .input(inputs::ChessBucketsMirrored::new(INPUT_BUCKETS))
+        .output_buckets(outputs::MaterialCount::<OUTPUT_BUCKETS>)
+        .feature_transformer(HIDDEN_SIZE)
+        .activate(Activation::SCReLU)
+        .add_layer(1)
+        .build();
+
+    let schedule = TrainingSchedule {
+		// name of checkpoint               
+        net_id: "leorik".to_string(),
+        eval_scale: SCALE as f32,
+        steps: TrainingSteps {
+            batch_size: 16_384,
+            batches_per_superbatch: 6104,
+            start_superbatch: 1,
+			// training stops after reaching this
+            end_superbatch: 1200,
+        },
+		// This controls the mix of WDL/Eval used, 1.0 = all WDL
+        wdl_scheduler: wdl::ConstantWDL { value: 1.0 },
+		// Learning rate schedule, this means it starts at 0.001,
+		// and every 15 epochs you multiply it by 0.3
+        lr_scheduler: lr::StepLR { start: 0.001, gamma: 0.75, step: 50 },
+        save_rate: 40,
+    };
+
+    trainer.set_optimiser_params(optimiser::AdamWParams::default());
+
+    let settings = LocalSettings { threads: 4, test_set: None, output_directory: "checkpoints", batch_queue_size: 64 };
+
+    // loading directly from a `BulletFormat` file
+    let data_loader = loader::DirectSequentialDataLoader::new(&[
+	"F:/TD3/FiSh_5K_Q5_v13-FRCv1_01.bullet.bin",
+	"F:/TD3/FiSh_5K_Q5_v13-FRCv1_02.bullet.bin",
+	"F:/TD3/FiSh_5K_Q5_v13-FRCv1_03.bullet.bin",
+	"F:/TD3/FiSh_5K_Q5_v13-FRCv1_04.bullet.bin",
+	]);
+
+	//trainer.load_from_checkpoint("leorik-360x");
+    trainer.run(&schedule, &settings, &data_loader);
+}
